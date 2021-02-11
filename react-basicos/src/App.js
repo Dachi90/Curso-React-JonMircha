fa045a4067a1bc924ffd7e2728c5dafd0ac6446b
Martin <bergesio90@gmail.com>
2021-02-11 19:52:38 +0100

    Video 3. Sintaxis JSX

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let nombre = "Jon";
+  let auth = false;
+  let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" />
+        <h1>{nombre}</h1>
+        <p>{auth ? "El usuario esta logeado" : "El usuario no esta logeado"}</p>
+        <p>{2 + 1}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <ul>
+          {estaciones.map((el, index) => (
+            <li key={index}>{el}</li>
+          ))}
+        </ul>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
