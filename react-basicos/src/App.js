bab173815d3229f83f8d3c979b699433719688b5
Martin <bergesio90@gmail.com>
2021-02-20 17:58:03 +0100

    Video 6. Estado

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Componente from "./components/Componente";
 import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
           <Componente msg="Hola soy un Componente Funcional Expresado desde una prop" />
           <hr />
           <Propiedades cadena="Esto es una cadena de texto" numero={19} booleano={false} arreglo={[1, 2, 3]} objeto={{ nombre: "Jon", correo: "jonmircha@gmail.com" }} funcion={(num) => num * num} elementoReact={<i>Esto es un elemento React</i>} componenteReact={<Componente msg="Soy un componente pasado como Prop" />} />
+          <hr />
+          <Estado />
         </section>
       </header>
     </div>
