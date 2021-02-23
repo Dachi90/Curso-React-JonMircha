6f95f979114ddc365c0a7a67e0dab6430986cee5
Martin <bergesio90@gmail.com>
2021-02-23 19:26:50 +0100

    Video 8. Renderizado de Elementos

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -5,6 +5,7 @@ import Componente from "./components/Componente";
 import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
           <Estado />
           <hr />
           <RenderizadoCondicional />
+          <hr />
+          <RenderizadoElementos />
         </section>
       </header>
     </div>
