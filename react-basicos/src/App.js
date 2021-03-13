f5b2d333c02f5f03bce444935bace066a55159d8
Martin <bergesio90@gmail.com>
2021-03-13 15:53:15 +0100

    Video 22. Formularios

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -15,6 +15,7 @@ import RelojHooks from "./components/RelojHooks";
 import AjaxHooks from "./components/AjaxHooks";
 import HooksPersonalizados from "./components/HooksPersonalizados";
 import Referencias from "./components/Referencias";
+import Formularios from "./components/Formularios";
 
 function App() {
   return (
@@ -61,6 +62,9 @@ function App() {
           <HooksPersonalizados />
           <hr />
           <Referencias />
+          <hr />
+          <Formularios />
+          <br /> <br /> <br /> <br />
         </section>
       </header>
     </div>
