e7fd55bebfbe4b609e94effa00059f3edc61c40d
Martin <bergesio90@gmail.com>
2021-04-11 17:08:16 +0200

    Video 35. CRUD API ( 4 / 5 )

--- react-ejercicios/src/components/CrudApi.js
+++ react-ejercicios/src/components/CrudApi.js
@@ -2,23 +2,34 @@ import React, { useState, useEffect } from "react";
 import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   let api = helpHttp();
   let url = "http://localhost:5000/santos";
 
   useEffect(() => {
-    api.get(url).then((res) => {
-      console.log(res);
-      if (!res.err) {
-        setDb(res);
-      } else {
-        setDb(null);
-      }
-    });
-  }, []);
+    setLoading(true);
+    helpHttp()
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        if (!res.err) {
+          setDb(res);
+          setError(null);
+        } else {
+          setDb(null);
+          setError(res);
+        }
+
+        setLoading(false);
+      });
+  }, [url]);
 
   const createData = (data) => {
     data.id = Date.now();
@@ -45,7 +56,9 @@ const CrudApi = () => {
       <h2>CRUD API </h2>
       <article className="grid-1-2">
         <CrudForm createData={createData} updateData={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit} />
-        <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />
+        {loading && <Loader />}
+        {error && <Message msg={`Error ${error.status}: ${error.statusText}`} bgColor={"#dc3545"} />}
+        {db && <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />}
       </article>
     </div>
   );
