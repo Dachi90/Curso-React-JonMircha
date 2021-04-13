a2f8551b70f78f84725041d8d03359b409d04b67
Martin <bergesio90@gmail.com>
2021-04-13 20:18:44 +0200

    Video 36. CRUD API: ( 5 / 5 )

--- react-ejercicios/src/components/CrudApi.js
+++ react-ejercicios/src/components/CrudApi.js
@@ -18,7 +18,7 @@ const CrudApi = () => {
     helpHttp()
       .get(url)
       .then((res) => {
-        console.log(res);
+        //console.log(res);
         if (!res.err) {
           setDb(res);
           setError(null);
@@ -33,20 +33,60 @@ const CrudApi = () => {
 
   const createData = (data) => {
     data.id = Date.now();
-    setDb([...db, data]);
+    //console.log(data)
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.post(url, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        setDb([...db, res]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    let endpoint = `${url}/${data.id}`;
+    //console.log(endpoint);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = (id) => {
     let isDelete = window.confirm(`¿Estás seguro de eliminar el registro con el id '${id}'?`);
 
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
