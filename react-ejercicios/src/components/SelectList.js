b67cdcecd2f3cf878b57e0f537ce7ee89c5efeb2
Martin <bergesio90@gmail.com>
2021-04-22 19:24:17 +0200

    Video 44. Selects Anidados: ( 3 / 3 )

--- react-ejercicios/src/components/SelectList.js
+++ react-ejercicios/src/components/SelectList.js
@@ -1,9 +1,29 @@
-export const SelectList = () => {
+import { useFetch } from "../hooks/useFetch";
+import Loader from "./Loader";
+import Message from "./Message";
+
+export const SelectList = ({ title, url, handleChange }) => {
+  const { data, error, loading } = useFetch(url);
+  //console.log(data, error, loading);
+
+  if (!data) return null;
+  if (error) {
+    return <Message msg={`Error ${error.status}: ${error.status.text}`} bgColor="#dc3545" />;
+  }
+
+  let id = `select-${title}`;
+  let label = title.charAt(0).toUpperCase() + title.slice(1) + ": ";
+  let options = data.response[title];
+  //console.log(options);
+
   return (
-    <div>
-      <select name="" id="">
-        <option value="">---</option>
+    <>
+      <label htmlFor={id}>{label}</label>
+      {loading && <Loader />}
+      <select name={id} id={id} onChange={handleChange}>
+        <option value="">Elige un {title}</option>
+        {data && options.map((el) => <option value={el}>{el}</option>)}
       </select>
-    </div>
+    </>
   );
 };
--- react-ejercicios/src/components/SelectsAnidados.js
+++ react-ejercicios/src/components/SelectsAnidados.js
@@ -6,21 +6,23 @@ const SelectsAnidados = () => {
   const [town, setTown] = useState("");
   const [suburb, setSuburb] = useState("");
 
+  const TOKEN = "d81a7ac7-976d-4e1e-b7d3-b1979d791b6c";
+
   return (
     <div>
       <h2>Selects Anidados</h2>
       <h3>México</h3>
       <SelectList
-        title="Estados"
-        url=""
+        title="estado"
+        url={`https://api-sepomex.hckdrk.mx/query/get_estados?token=${TOKEN}`}
         handleChange={(e) => {
           setState(e.target.value);
         }}
       />
       {state && (
         <SelectList
-          title="Municipiios"
-          url=""
+          title="municipios"
+          url={`https://api-sepomex.hckdrk.mx/query/get_municipio_por_estado/${state}?token=${TOKEN}`}
           handleChange={(e) => {
             setTown(e.target.value);
           }}
@@ -28,8 +30,8 @@ const SelectsAnidados = () => {
       )}
       {town && (
         <SelectList
-          title="Colonias"
-          url=""
+          title="colonia"
+          url={`https://api-sepomex.hckdrk.mx/query/get_colonia_por_municipio/${town}?token=${TOKEN}`}
           handleChange={(e) => {
             setSuburb(e.target.value);
           }}
