297198c55f9026696de810b6aae131cd671c3786
Martin <bergesio90@gmail.com>
2021-05-04 19:45:56 +0200

    Video 51. Ventana Modal: ( 3 / 3 )

--- react-ejercicios/src/components/Modals.js
+++ react-ejercicios/src/components/Modals.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { useModal } from "../hooks/useModal";
+import ContactForm from "./ContactForm";
 import Modal from "./Modal";
+import SongSearch from "./SongSearch";
 
 const Modals = () => {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenModalContact, openModalContact, closeModalContact] = useModal(false);
+  const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
   return (
     <div>
       <h2>Modales</h2>
-      <button>Modal 1</button>
-      <Modal>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3>Modal 1</h3>
         <p>Hola este es el contenido de mi modal 1</p>
         <img src="https://placeimg.com/400/400/animals" alt="Animal" />
       </Modal>
-      <button>Modal 2</button>
-      {/* <Modal>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3>Modal 2</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste commodi nemo debitis voluptatum iusto ut saepe asperiores! Eaque natus quas veritatis neque odit cum. Tempora tempore quisquam soluta nulla iure?</p>
         <img src="https://placeimg.com/400/400/nature" alt="nature" />
-      </Modal> */}
+      </Modal>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenModalContact} closeModal={closeModalContact}>
+        <ContactForm />
+      </Modal>
+      <button onClick={openModalSong}>Modal Buscador de canciones</button>
+      <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
+        <SongSearch />
+      </Modal>
     </div>
   );
 };
