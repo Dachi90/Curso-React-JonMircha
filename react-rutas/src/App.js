e0c402e0ea51a7005120cedff383cb60ee88fb38
Martin <bergesio90@gmail.com>
2021-05-11 19:38:19 +0200

    Video 53. React Router: Introducción

---
+++ react-rutas/src/App.js
@@ -0,0 +1,16 @@
+import ConceptosBasicos from "./Components/ConceptosBasicos";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router</h1>
+      <a href="http://reactrouter.com/web/guides/quick-start" target="_blank" rel="noreferrer">
+        Documentación
+      </a>
+      <hr />
+      <ConceptosBasicos />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-rutas/src/Components/ConceptosBasicos.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const ConceptosBasicos = () => {
+  return (
+    <div>
+      <h2>Conceptos básicos</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h3>Home</h3>
+            <p>Bienvenidos al tema de las Rutas en React</p>
+          </Route>
+          <Route exact path="/acerca">
+            <h3>Acerca</h3>
+          </Route>
+          <Route exact path="/contacto">
+            <h3>Contacto</h3>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default ConceptosBasicos;
