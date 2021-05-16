2ea84c60d2a1caab4c7bcdb7fd38ccabe225682d
Martin <bergesio90@gmail.com>
2021-05-16 09:57:44 +0200

    Video 56. React Router: Componentes Link y NavLink

---
+++ react-rutas/src/Components/MenuConceptos.js
@@ -0,0 +1,40 @@
+import { Link, NavLink } from "react-router-dom";
+
+const MenuConceptos = () => {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <span>Menú con enlaces html (no recomendado):</span>
+          <a href="/">Home</a>
+          <a href="/contacto">Contacto</a>
+          <a href="/acerca">Acerca</a>
+        </li>
+        <li>
+          <span>Componente Link:</span>
+          <Link to="/">Home</Link>
+          <Link to="/acerca">Acerca</Link>
+          <Link to="/contacto">Contacto</Link>
+          <Link to="/no-existe">No existe</Link>
+        </li>
+        <li>
+          <span>Componente NavLink :</span>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink exact to="/acerca" activeClassName="active">
+            Acerca
+          </NavLink>
+          <NavLink exact to="/contacto" activeClassName="active">
+            Contacto
+          </NavLink>
+          <NavLink exact to="/no-existe" activeClassName="active">
+            No existe
+          </NavLink>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+export default MenuConceptos;
