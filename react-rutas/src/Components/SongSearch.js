0f22caeb35cbad4a93cce03e2db2fcdea51be8f3
Martin PCescritorio <bergesio90@gmail.com>
2021-10-30 15:04:31 +0200

    68 Buscador Canciones localStorage ( 2 / 5 )

--- react-rutas/src/Components/SongSearch.js
+++ react-rutas/src/Components/SongSearch.js
@@ -6,11 +6,14 @@ import Loader from "./Loader";
 import SongDetails from "./SongDetails";
 import SongForm from "./SongForm";
 
+let mySongsInit = JSON.parse(localStorage.getItem("mySongs")) || [];
+
 const SongSearch = () => {
   const [search, setSearch] = useState(null);
   const [bio, setBio] = useState(null);
   const [lyric, setLyric] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [mySongs, setMySongs] = useState(mySongsInit);
 
   useEffect(() => {
     if (search === null) return;
@@ -35,16 +38,23 @@ const SongSearch = () => {
     };
 
     fetchData();
-  }, [search]);
+
+    localStorage.setItem("mySongs", JSON.stringify(mySongs));
+  }, [search, mySongs]);
 
   const handleSearch = (data) => {
     //console.log(data);
     setSearch(data);
   };
 
+  const handleSaveSong = () => {
+    alert("Salvando canción en favoritos");
+  };
+
+  const handleDeleteSong = (id) => {};
+
   return (
     <div>
-      <h2>SongSearch</h2>
       <HashRouter basename="/canciones">
         <header>
           <h2>Song Search</h2>
@@ -54,7 +64,7 @@ const SongSearch = () => {
         <article className="grid-1-3">
           <Switch>
             <Route exact path="/">
-              <SongForm handleSearch={handleSearch} />
+              <SongForm handleSearch={handleSearch} handleSaveSong={handleSaveSong} />
               <h2>Tabla de canciones</h2>
               {search && !loading && <SongDetails search={search} bio={bio} lyric={lyric} />}
             </Route>
