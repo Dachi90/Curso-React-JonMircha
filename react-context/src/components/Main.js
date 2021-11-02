1ecaba6a215a39fab6ba8a308ce6cb2972b690c5
Martin Bergesio Trabajo <bergesio90@gmai.com>
2021-11-02 17:30:31 +0100

    76. Context API - Haciendo una APP con THEME

---
+++ react-context/src/components/Main.js
@@ -0,0 +1,11 @@
+const Main = ({ theme }) => {
+  return (
+    <main className={theme}>
+      <p>Bienvenido invitado</p>
+      <p>Hola Usuario</p>
+      <p>Mi contenido prinipal</p>
+    </main>
+  );
+};
+
+export default Main;
