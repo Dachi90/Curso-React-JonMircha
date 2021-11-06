d5fd1e89aa6c0ade66c37b4c7ce1ced01515437f
Martin PCescritorio <bergesio90@gmail.com>
2021-11-06 14:46:01 +0100

    83. CRUD API Context (2 / 2) ANTES DE VER EL VIDEO

--- react-context/src/components/CrudApi.js
+++ react-context/src/components/CrudApi.js
@@ -1,24 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { CrudProvider } from "../context/CrudContext";
-import { helpHttp } from "../helpers/helpHttp";
+import { useContext } from "react";
+import CrudContext from "../context/CrudContext";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
 import Loader from "./Loader";
 import Message from "./Message";
 
 const CrudApi = () => {
+  const { loading, error, db } = useContext(CrudContext);
   return (
-    <CrudProvider>
-      <div>
-        <h2>CRUD API con Context API</h2>
-        <article className="grid-1-2">
-          <CrudForm />
-          {loading && <Loader />}
-          {error && <Message msg={`Error ${error.status}: ${error.statusText}`} bgColor={"#dc3545"} />}
-          {db && <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />}
-        </article>
-      </div>
-    </CrudProvider>
+    <div>
+      <h2>CRUD API con Context API</h2>
+      <article className="grid-1-2">
+        <CrudForm />
+        {loading && <Loader />}
+        {error && <Message msg={`Error ${error.status}: ${error.statusText}`} bgColor={"#dc3545"} />}
+        {db && <CrudTable />}
+      </article>
+    </div>
   );
 };
 
--- react-context/src/components/CrudTable.js
+++ react-context/src/components/CrudTable.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import CrudContext from "../context/CrudContext";
 import CrudTableRow from "./CrudTableRow";
 
-const CrudTable = ({ data, setDataToEdit, deleteData }) => {
+const CrudTable = () => {
+  const { db } = useContext(CrudContext);
   return (
     <div>
       <h3>Tabla de Datos</h3>
@@ -15,8 +17,8 @@ const CrudTable = ({ data, setDataToEdit, deleteData }) => {
         </thead>
 
         <tbody>
-          {data.length > 0 ? (
-            data.map((el) => <CrudTableRow key={el.id} el={el} setDataToEdit={setDataToEdit} deleteData={deleteData} />)
+          {db.length > 0 ? (
+            db.map((el) => <CrudTableRow key={el.id} el={el} />)
           ) : (
             <tr>
               <td colSpan="3">Sin datos</td>
